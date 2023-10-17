0e5e491cc14027fa7e41596ab6f6ced17b9e8fc5
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-10-17 23:06:46 +0530

    Update transactionMintStatemine.ts

--- composables/transaction/mintToken/transactionMintStatemine.ts
+++ composables/transaction/mintToken/transactionMintStatemine.ts
@@ -45,7 +45,8 @@ export const prepareTokenMintArgs = async (token: TokenToMint & id, api) => {
   )
 
   const meta = api.tx.nfts.setMetadata(collectionId, nextId, metadata)
-  //const Airdrop = api.tx.nfts.transfer(collectionId, nextId, Airdrop )
+  console.log(collectionId)
+  const Airdrop = api.tx.nfts.transfer( collectionId, nextId, Airdrop)
 
   const list =
     Number(price) > 0
