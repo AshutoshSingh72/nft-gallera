162d53f633942e717d58192dbfc3b5ae5fb8e238
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-10-18 22:47:32 +0530

    Update CreateNft.vue

--- components/create/CreateNft.vue
+++ components/create/CreateNft.vue
@@ -111,7 +111,7 @@
 
      <!--Airdrop to an address-->
       <NeoField
-        v-if="isAssetHub ? ahp"
+        v-if="isAssetHub?"
         :key="currentChain"
         :label="$t('Airdrop NFT')"
         required
