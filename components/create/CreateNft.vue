d43a33a8534b2423925ac2169c323ff64f37949a
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-10-18 22:14:54 +0530

    Update CreateNft.vue

--- components/create/CreateNft.vue
+++ components/create/CreateNft.vue
@@ -124,7 +124,7 @@
       
       <!-- Airdrop -->
        <NeoField 
-         v-if="form.Airdropnft"
+         v-if="form.Airdropnft" v-if="!isAssetHub"
         :label="`${$t('Receiver address')} *`"
        required
         :error="!form.Airdrop" >
