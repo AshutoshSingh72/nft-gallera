c88c6260497f1569801c3abcafcdae2d9073fc16
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-11-03 00:23:59 +0530

    Update IdentityForm.vue

--- components/common/IdentityForm.vue
+++ components/common/IdentityForm.vue
@@ -293,6 +293,7 @@ const setIdentityValue = (values: Record<string, string>) => {
 const hasIdentity = computed(() => {
   const { display, legal, web, twitter, riot, email } = identityData.value
   return (
+    this.Createidentity = 'Update Identity',
     accountId.value &&
     Boolean(display || legal || web || twitter || riot || email)
   )
