669deea8eb5cde9c418adadb39af6741727fef60
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-11-04 11:44:22 +0530

    Update IdentityForm.vue

--- components/common/IdentityForm.vue
+++ components/common/IdentityForm.vue
@@ -290,14 +290,19 @@ const setIdentityValue = (values: Record<string, string>) => {
 }
 
 const hasIdentity = computed(() => {
-  const { display, legal, web, twitter, riot, email } = identityData.value,
-    var Createidentity = 'Create Identity'
+  const { display, legal, web, twitter, riot, email } = identityData.value
   return (
-    this.Createidentity = "Update Identity",
     accountId.value &&
     Boolean(display || legal || web || twitter || riot || email)
   )
 })
+var Createidentity = 'Create Identity' {
+  if(hasIdentity == true){
+    return (
+      this.Createidentity = 'lol'
+      )
+  }
+}
 
 const handleUrlPrefixChange = async () => {
   deposit.value = await fetchDeposit()
