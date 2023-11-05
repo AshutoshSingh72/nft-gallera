32edb2f3f6e9fcb5b8aa085ca98d4b40cddedeff
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-11-05 19:25:02 +0530

    Update useDeposit.ts

--- composables/useDeposit.ts
+++ composables/useDeposit.ts
@@ -28,7 +28,7 @@ export default function (prefix: ComputedRef<Prefix>) {
 
   const chainSymbol = ref('')
 
-  export watchEffect(async (token: TokenToMint) => {
+  watchEffect(async (token: TokenToMint) => {
     const { copies } = token
     if (prefix.value) {
       const api = await apiInstanceByPrefix(prefix.value)
@@ -60,7 +60,7 @@ export default function (prefix: ComputedRef<Prefix>) {
         false
       )
       totalItemDeposit.value = format(
-        metadataDeposit.value + itemDeposit.value * copies, + existentialDeposit.value,
+        metadataDeposit.value + itemDeposit.value + copies.value + existentialDeposit.value,
         chain.tokenDecimals,
         false
       )
