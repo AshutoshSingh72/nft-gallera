b20531e3820cfb722fff7994617919a2a9bc91cd
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-11-05 20:34:30 +0530

    Update useDeposit.ts

--- composables/useDeposit.ts
+++ composables/useDeposit.ts
@@ -60,7 +60,8 @@ export default function (prefix: ComputedRef<Prefix>) {
         false
       )
       totalItemDeposit.value = format(
-        metadataDeposit.value + itemDeposit.value + copies + existentialDeposit.value,
+        metadataDeposit.value + itemDeposit.value + existentialDeposit.value,
+        * copies,
         chain.tokenDecimals,
         false
       )
