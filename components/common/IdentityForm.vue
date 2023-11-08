aa32b7c3682db9541bd4672fb53eeab4111a0fc8
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-11-08 18:41:16 +0530

    Update IdentityForm.vue

--- components/common/IdentityForm.vue
+++ components/common/IdentityForm.vue
@@ -384,11 +384,11 @@ watch(urlPrefix, (value) => {
   selectChain.value = value
 })
 
-  const currentChain = computed(() => selectChain.value as Prefix, watch(currentChain() => {
+  const currentChain = computed(() => selectChain.value as Prefix,
   if (currentChain.value !== urlPrefix.value) {
     setUrlPrefix(currentChain.value as Prefix)
   }
-}))
+)
 const { isAssetHub } = useIsChain(currentChain)
 
   
